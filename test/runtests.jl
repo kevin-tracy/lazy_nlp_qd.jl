c2ed77aa58a70d42b827c845b8015a6ba23229d6
Kevin Tracy <kevt.laser@gmail.com>
2024-03-20 08:07:59 -0700

    cleaned up tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,17 +8,14 @@ import ForwardDiff as FD
 import Random
 Random.seed!(1234)
 
-# @testset "fmincon_sparse_tests" begin 
-#     include("fmincon_sparse_test.jl")
-# end
+@testset "fmincon_sparse_tests" begin 
+    include("fmincon_sparse_test.jl")
+end
 
-# @testset "sparse trajopt test" begin 
-#     include("trajopt_test.jl")
-# end
+@testset "sparse trajopt test" begin 
+    include("trajopt_test.jl")
+end
 
-# right now I can't export this without overwriting 
-# a bunch of custom MOI methods that are specific 
-# to sparse/dense. I need to figure out a way around.
 @testset "fmincon_dense_tests" begin 
     include("fmincon_dense_test.jl")
 end
